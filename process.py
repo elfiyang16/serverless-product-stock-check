3a0babe61d4b20edf3629365c36627f462134207
Elfi Yang <elfiyang16@gmail.com>
2021-02-15 20:17:16 +0000

    feat: add download function and refactor

--- process.py
+++ process.py
@@ -9,7 +9,10 @@ import os
 from io import StringIO
 import io
 import boto3
+import botocore
 import yaml
+from datetime import datetime
+
 
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
@@ -61,10 +64,10 @@ def data_load(values):
     df = pd.DataFrame(values, columns=headers)
     return df
   
-def aws_session(region_name='eu-west-1'):
+def aws_session():
     return boto3.session.Session(aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                                 aws_secret_access_key=os.getenv('AWS_ACCESS_KEY_SECRET'),
-                                region_name=region_name)
+                                region_name=os.getenv('REGION'))
     
 
 def make_bucket(name, acl):
@@ -73,19 +76,32 @@ def make_bucket(name, acl):
     return s3_resource.create_bucket(Bucket=name, ACL=acl)
   
   
-def upload_to_bucket(df, FILE_NAME):
-    # Upload to S3
+def upload_to_bucket(df, FILE_NAME, BUCKET_NAME='gsheets-product-data'):
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index = False, header = True)
 
     session = aws_session()
     s3 = session.resource('s3')
-    s3.Bucket('gsheets-product-data').put_object(Key=FILE_NAME, Body=csv_buffer.getvalue())
-    print('csv uploaded to s3://gsheets-data/%s' %(FILE_NAME,))
+    s3.Bucket(BUCKET_NAME).put_object(Key=FILE_NAME, Body=csv_buffer.getvalue())
+    print(f"csv uploaded to s3://{BUCKET_NAME}/{FILE_NAME}")
 
+def download_from_bucket(FILE_NAME, BUCKET_NAME='gsheets-product-data'):
+    session = aws_session()
+    s3 = session.client('s3')
+    
+    try:
+      obj = s3.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
+    except botocore.exceptions.ClientError as e:
+      if e.response['Error']['Code'] == "404":
+        print("The file does not exist.")
+      else:
+        raise
+    df = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='utf8')
+    df.to_json (path_or_buf=f'./downloaded/{str(datetime.now())}.json')
+    print(f'download csv object {FILE_NAME} from {BUCKET_NAME}')
+  
 
 if __name__ == '__main__':
-    # Reads YAML file for all spreadsheets
     file = open("sheets.yml")
     sources =  yaml.load(file, Loader=yaml.FullLoader)
     file.close()
@@ -94,3 +110,4 @@ if __name__ == '__main__':
         values = gsheets(x['spreadsheet_id'], x['range_name'])
         df = data_load(values)
         upload_to_bucket(df, x['filename'])
+        download_from_bucket(x['filename'])
