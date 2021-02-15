69d4fed3e2b60799a9c2efec645dbb5f2fbcb03e
Elfi Yang <elfiyang16@gmail.com>
2021-02-15 19:18:56 +0000

    feat: add to upload to s3 bucket

--- process.py
+++ process.py
@@ -11,6 +11,9 @@ import io
 import boto3
 import yaml
 
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 
@@ -52,3 +55,42 @@ def gsheets(SPREADSHEET_ID, RANGE_NAME):
         
     return values
   
+def data_load(values):
+    # Insert into DataFrame
+    headers = values.pop(0)
+    df = pd.DataFrame(values, columns=headers)
+    return df
+  
+def aws_session(region_name='eu-west-1'):
+    return boto3.session.Session(aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                aws_secret_access_key=os.getenv('AWS_ACCESS_KEY_SECRET'),
+                                region_name=region_name)
+    
+
+def make_bucket(name, acl):
+    session = aws_session()
+    s3_resource = session.resource('s3')
+    return s3_resource.create_bucket(Bucket=name, ACL=acl)
+  
+  
+def upload_to_bucket(df, FILE_NAME):
+    # Upload to S3
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index = False, header = True)
+
+    session = aws_session()
+    s3 = session.resource('s3')
+    s3.Bucket('gsheets-product-data').put_object(Key=FILE_NAME, Body=csv_buffer.getvalue())
+    print('csv uploaded to s3://gsheets-data/%s' %(FILE_NAME,))
+
+
+if __name__ == '__main__':
+    # Reads YAML file for all spreadsheets
+    file = open("sheets.yml")
+    sources =  yaml.load(file, Loader=yaml.FullLoader)
+    file.close()
+
+    for x in sources.values():
+        values = gsheets(x['spreadsheet_id'], x['range_name'])
+        df = data_load(values)
+        upload_to_bucket(df, x['filename'])
